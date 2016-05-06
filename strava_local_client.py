f7f3bba461f911990184ce686e482500240ce0b7
Barry <barrald1213@gmail.com>
2016-05-06 10:58:05 -0400

    working first version

---
+++ strava_local_client.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+"""
+Strava Development Sandbox.
+
+Get your *Client ID* and *Client Secret* from https://www.strava.com/settings/api
+
+Usage:
+  strava_local_client.py get_write_token <client_id> <client_secret> [options]
+  strava_local_client.py find_settings
+
+Options:
+  -h --help      Show this screen.
+  --port=<port>  Local port for OAuth client [default: 8000].
+"""
+
+import stravalib
+from flask import Flask, request
+
+app = Flask(__name__)
+
+API_CLIENT = stravalib.Client()
+
+# set these in __main__
+CLIENT_ID = None
+CLIENT_SECRET = None
+
+@app.route("/auth")
+def auth_callback():
+    code = request.args.get('code')
+    access_token = API_CLIENT.exchange_code_for_token(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        code=code
+        )
+    return access_token
+
+
+if __name__ == '__main__':
+    import docopt
+    import subprocess
+    import sys
+    from blessings import Terminal
+
+    args = docopt.docopt(__doc__)
+    t = Terminal()
+
+    if args['get_write_token']:
+        CLIENT_ID, CLIENT_SECRET = args['<client_id>'], args['<client_secret>']
+        auth_url = API_CLIENT.authorization_url(
+            client_id=args['<client_id>'],
+            redirect_uri='http://127.0.0.1:{port}/auth'.format(port=args['--port']),
+            scope='view_private,write',
+            state='from_cli'
+            )
+        if sys.platform == 'darwin':
+            print(t.green('On OS X - launching {0} at default browser'.format(auth_url)))
+            subprocess.call(['open', auth_url])
+        else:
+            print(t.red('Go to {0} to authorize access: '.format(auth_url)))
+        app.run(port=int(args['--port']))
+    elif args['find_settings']:
+        subprocess.call(['open', 'https://www.strava.com/settings/api'])
---
+++ uploader.py
@@ -0,0 +1,136 @@
+import os
+from stravalib import Client, exc, model
+from requests.exceptions import ConnectionError, HTTPError
+import requests
+import csv
+import shutil
+import datetime as dt
+from datetime import datetime
+
+# Creating a log file and a logging function
+log = open("log.txt","a+")
+def logger (message):
+	now = str(datetime.now())
+	log.write(now + " | " + message + "\n")
+	print message
+
+# Opening the connection to Strava
+logger("Connecting to Strava")
+client = Client()
+
+# You need to run the strava_local_client.py script - with your application's ID and secret - to generate the access token.
+access_token = "0649a9c5861f34bbc79410c39f470a130df52da7"
+client.access_token = access_token
+athlete = client.get_athlete()
+logger("Now authenticated for " + athlete.firstname + " " + athlete.lastname)
+
+# Creating an archive folder to put uploaded .gpx files
+archive = "../archive"
+
+# Function to convert the HH:MM:SS in the Runkeeper CSV to seconds
+def duration_calc(duration):
+	
+	# Splits the duration on the :, so we wind up with a 3-part array
+	split_duration = str(duration).split(":")
+
+	# If the array only has 2 elements, we know the run was less than an hour
+	if len(split_duration) == 2:
+		hours = 0
+		minutes = int(split_duration[0])
+		seconds = int(split_duration[1])
+	else:
+		hours = int(split_duration[0])
+		minutes = int(split_duration[1])
+		seconds = int(split_duration[2])
+	
+	total_seconds = seconds + (minutes*60) + (hours*60*60)
+	return total_seconds
+
+activity_counter = 0
+# We open the cardioactivities CSV file and start reading through it
+with open('cardioActivities.csv', 'rb') as csvfile:
+	runs = csv.reader(csvfile)
+	for row in runs:
+		# if there is a gpx file listed, find it and upload it
+		if (".gpx" in row[11] and "Running" in row[1]):
+			gpxfile = row[11]
+			if gpxfile in os.listdir('.'):
+				logger("Uploading " + gpxfile)
+				try:
+					upload = client.upload_activity(
+						activity_file = open(gpxfile,'r'),
+						data_type = 'gpx',
+						private = False,
+						description = row[10],
+						activity_type = "run"
+						)
+				except exc.ActivityUploadFailed as err:
+					logger("Uploading problem raised: {}".format(err))
+					errStr = str(err)
+					# deal with duplicate type of error, if duplicate then continue with next file, else stop
+					if errStr.find('duplicate of activity'):
+						logger("Moving dulicate activity file {}".format(gpxfile))
+						shutil.move(gpxfile,archive)
+						isDuplicate = True
+						log_message = "Duplicate File " + gpxfile
+						log.write(log_message)
+					else:
+						exit(1)
+
+				except ConnectionError as err:
+					logger("No Internet connection: {}".format(err))
+					exit(1)
+
+				logger("Upload succeeded.\nWaiting for response...")
+
+				try:
+					upResult = upload.wait()
+				except HTTPError as err:
+					logger("Problem raised: {}\nExiting...".format(err))
+					exit(1)
+				except:
+					logger("Another problem occured, sorry...")
+					exit(1)
+				
+				logger("Uploaded " + gpxfile + " - Activity id: " + str(upResult.id))
+
+				shutil.move(gpxfile, archive)
+			else:
+				logger("No file found for " + gpxfile + "!")
+
+		#if no gpx file, upload the data from the CSV
+		else:
+			if (("Running" in row[1]) and (row[0] not in log)):
+				logger("Manually uploading " + row[0])
+				dur = duration_calc(row[4])
+				dist = float(row[3])*1609.344
+				starttime = datetime.strptime(str(row[0]),"%Y-%m-%d %H:%M:%S")
+
+				# designates part of day for name assignment above, matching Strava convention for GPS activities
+				if 3 <= starttime.hour <= 11:
+					part = "Morning"
+				elif 12 <= starttime.hour <= 4:
+					part = "Afternoon"
+				elif 5 <= starttime.hour <=7:
+					part = "Evening"
+				else:
+					part = "Night"
+				
+				try:
+					upload = client.create_activity(
+						name = part + " Run (Manual)",
+						start_date_local = starttime,
+						elapsed_time = dur,
+						distance = dist,
+						description = row[10],
+						activity_type = "run"
+						)
+
+					logger("Manually created " + row[0])
+
+				except ConnectionError as err:
+					logger("No Internet connection: {}".format(err))
+					exit(1)	
+		activity_counter += 1
+
+	logger("Complete! Logged " + str(activity_counter) + " activities.")
